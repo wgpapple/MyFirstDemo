a3b390fe7b1d2ee5c6083d8e11786f599d4eca5b
wgp <>
2018-11-22 22:14:50 +0800

    first commit

---
+++ app/src/main/java/com/sjkj/appframe/myfirstdemo/MyTestUtils.java
@@ -0,0 +1,17 @@
+package com.sjkj.appframe.myfirstdemo;
+
+/**
+ * Created by admin on 2018/11/22.
+ */
+
+public class MyTestUtils {
+
+    public void test1() {
+        System.out.println(">>>test1");
+    }
+
+    public void printSuccess() {
+        System.out.println(">>>你的github已配置成功");
+    }
+
+}
---
+++ fistmoudule/src/main/java/com/sjkj/appframe/fistmoudule/MyMouduleTestUtils.java
@@ -0,0 +1,20 @@
+package com.sjkj.appframe.fistmoudule;
+
+import android.app.Application;
+import android.widget.Toast;
+
+/**
+ * Created by admin on 2018/11/22.
+ */
+
+public class MyMouduleTestUtils {
+
+    public static void test1() {
+        System.out.println(">>>test1");
+    }
+
+    public static void printSuccess() {
+        System.out.println(">>>你的github已配置成功");
+    }
+
+}
